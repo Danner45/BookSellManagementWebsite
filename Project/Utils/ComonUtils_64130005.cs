d51de9bda2328a923296217cc9e6be3fc73945a6
Danne132 <163326801+Danner45@users.noreply.github.com>
2025-01-14 19:36:47 +0700

    up

---
+++ Project/Utils/ComonUtils_64130005.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace Project_64130005.Utils
+{
+    public class ComonUtils_64130005
+    {
+        public static string HashPassword(string password)
+        {
+            try
+            {
+                // Create an instance of MD5
+                using (MD5 md5 = MD5.Create())
+                {
+                    // Compute hash of the input string
+                    byte[] messageDigest = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                    // Convert the byte array to a hexadecimal string
+                    StringBuilder sb = new StringBuilder();
+                    foreach (byte b in messageDigest)
+                    {
+                        sb.Append(b.ToString("x2")); // Convert each byte to a two-character hexadecimal string
+                    }
+
+                    return sb.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error generating hash: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Project/Utils/ProductSort.cs
@@ -0,0 +1,51 @@
+﻿using Project_64130005.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_64130005.Utils
+{
+    public class ProductSort
+    {
+        private static IProductSort tempSort;
+
+        //Đoạn code sắp xếp sp theo thứ tự tăng dần Ascending
+        public static IEnumerable<Sach> Ascending(IEnumerable<Sach> sachs)
+        {
+            //TempSort lưu trự một đối tượng
+            tempSort = new SortAscending();
+            return tempSort.Sort(sachs);
+        }
+
+        //Đoạn code sắp xếp sp theo thứ tự giảm dần Descending
+        public static IEnumerable<Sach> Descending(IEnumerable<Sach> sachs)
+        {
+            tempSort = new SortDescending();
+            return tempSort.Sort(sachs);
+        }
+
+        //Sắp xếp ds sản phẩm
+        public interface IProductSort
+        {
+            IEnumerable<Sach> Sort(IEnumerable<Sach> sachs);
+        }
+
+        //SortAscending sắp xếp danh sách sản phẩm theo giá đơn vị tăng dần.
+        public class SortAscending : IProductSort
+        {
+            public IEnumerable<Sach> Sort(IEnumerable<Sach> sachs)
+            {
+                return sachs.OrderBy(p => p.DonGia);
+            }
+        }
+        //SortDescending sắp xếp danh sách sản phẩm theo giá đơn vị giảm dần.
+        public class SortDescending : IProductSort
+        {
+            public IEnumerable<Sach> Sort(IEnumerable<Sach> sachs)
+            {
+                return sachs.OrderByDescending(p => p.DonGia);
+            }
+        }
+    }
+}
